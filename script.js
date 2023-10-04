31a48cfa2ee8cf1729f534c069ff7f7ca2b522f7
Diego Lopez <diego.lopez171205@gmail.com>
2023-10-04 09:02:56 -0700

    Added the flashcards project

--- script.js
+++ script.js
@@ -149,3 +149,56 @@ function calculate() {
         setTimeout(()=>{tip_amount.style.display = "none"},3000);
     }
 }
+
+//Flashcards
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
+const flashcards = document.getElementsByClassName("flashcards")[0];
+
+let contentArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+contentArray.forEach(divMaker)
+
+function divMaker(text) {
+    let div = document.createElement("div");
+    let h2_question = document.createElement("h2");
+    let h2_answer = document.createElement("h2");
+    div.className = 'flashcard';
+
+    h2_question.innerHTML = text.my_question;
+    h2_question.setAttribute('style', "border-top:1px solid red; padding: 15px; margin-top:30px;");
+
+    h2_answer.innerHTML = text.my_answer;
+    h2_answer.setAttribute('style', "text-align:center; display:none; color: red;");
+
+    div.appendChild(h2_answer);
+    div.appendChild(h2_question);
+
+    div.addEventListener("click", function() {
+        if(h2_answer.style.display == "none"){
+            h2_answer.style.display = "block";
+        } else{
+            h2_answer.style.display = "none";
+        }
+    })
+
+    flashcards.appendChild(div);
+}
+
+function addFlashcard() {
+    let flashcard_info = {
+        'my_question':question.value,
+        'my_answer':`Answer: ${answer.value}`
+    }
+
+    contentArray.push(flashcard_info);
+    localStorage.setItem('items', JSON.stringify(contentArray));
+    divMaker(contentArray[contentArray.length-1])
+    question.value = "";
+    answer.value = "";
+}
+
+function deleteFlashcards() {
+    localStorage.clear();
+    flashcards.innerHTML = ''
+    contentArray = [];
+}
